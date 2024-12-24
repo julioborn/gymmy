70f3f80515da2faa03e8bd9bf069af9d7f4555c6
julioborn <juliobornes10@gmail.com>
2024-12-24 17:55:15 -0300

    Solución de credenciales

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,17 +1,22 @@
 import mongoose from 'mongoose';
 
-const localURI = 'mongodb://localhost:27017/gimnasio'
-const atlasURI = 'mongodb+srv://julioborn:Estudiob123@gymmycluster.ooyeh.mongodb.net/gymmy';
-
-
 const connectMongoDB = async () => {
+    // Obtener el URI de la base de datos desde las variables de entorno
+    const uri = process.env.USE_ATLAS === "true" ? process.env.ATLAS_URI : process.env.MONGODB_URI;
+
+    if (!uri) {
+        throw new Error("No se encontró el URI para MongoDB. Verifica tus variables de entorno.");
+    }
+
+    // Evitar reconexiones múltiples
     if (mongoose.connection.readyState === 1) {
-        return; // Si ya estamos conectados
+        console.log("Ya conectado a MongoDB");
+        return;
     }
 
     try {
-        await mongoose.connect(localURI);
-        console.log("Conectado a la base de datos gimnasio");
+        await mongoose.connect(uri); // Sin necesidad de opciones adicionales
+        console.log(`Conectado a MongoDB en: ${uri}`);
     } catch (error) {
         console.error("Error de conexión a MongoDB:", error);
     }
