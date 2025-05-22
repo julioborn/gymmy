37449d055539aeb11011ba4b03f1bd461aaec37c
julioborn <juliobornes10@gmail.com>
2025-05-22 16:07:08 -0300

    emailPago 5

--- src/utils/emailPagoCuota.ts
+++ src/utils/emailPagoCuota.ts
@@ -8,7 +8,7 @@ export const enviarCorreoPagoCuota = async (email: string, nombre: string, pago:
         return;
     }
 
-    const recargoHtml = pago.recargo && Number(pago.recargo) > 0
+    const recargoHtml = Number(pago.recargo) > 0
         ?   `<li style="margin-bottom: 10px; font-size: 14px;">
                 <strong style="color: #000000;">Recargo aplicado:</strong> $${Number(pago.recargo).toFixed(2)}
             </li>`
