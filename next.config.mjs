741a02cae449dba3f6ce5b397f83d930650bfc05
julioborn <juliobornes10@gmail.com>
2025-05-22 12:52:48 -0300

    next-config 3

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const withPWA = nextPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development', 
+    disable: process.env.NODE_ENV === 'development', // Asegura que funcione solo en producción
 });
 
 export default withPWA({
-    // Aquí puedes añadir otras configuraciones de Next.js
+    // tus otras configuraciones aquí si tenés
 });
